2d0932253d3de160777b226c39f2b1ba7b982a7f
kaja <kaja.abbas@gmail.com>
2020-02-15 20:35:31 +0000

    Update vimc_hpv_run.R

--- code/vimc_hpv_run.R
+++ code/vimc_hpv_run.R
@@ -74,7 +74,7 @@ EstimateVaccineImpactVimcCentral <- function (vaccine_coverage_file,
   
   # read file -- central disease burden template
   vimc_template <- fread (disease_burden_template_file)
-  vimc_template <- vimc_template [country == "CHN"] # DEBUG -- remove this line later
+  # vimc_template <- vimc_template [country == "CHN" | country == "IND"] # DEBUG -- comment this line later
   
   # register batch data for vimc runs
   RegisterBatchDataVimc (vimc_coverage             = vimc_coverage, 
@@ -154,17 +154,219 @@ EstimateVaccineImpactVimcCentral <- function (vaccine_coverage_file,
 
 
 #-------------------------------------------------------------------------------
-# Generate vaccine impact estimates (sensitivity analysis/stochastic runs)
-EstimateVaccineImpactVimcStochastic <- function (diseaseBurdenCentralFile,
-                                                 resultsFile,
-                                                 psaDataFile,
-                                                 countryCode,
-                                                 diseaseBurdenStochasticFile,
-                                                 fileNumber) {
+#' Generate vaccine impact estimates (VIMC stochastic runs)
+#'
+#' Generate vaccine impact estimates for VIMC stochastic runs/sensitivity 
+#'   analysis. The inputs are central disease burden estimates, input 
+#'   parameter distributions (latin hyper sampling), runs forsensitivity analysis,
+#'   and filename for stochastic burden estimates. The outputs are stochastic 
+#'   disease burden estimates (full results file plus a file per country). 
+#'   
+#'   Stochastic disease burden estimates are generated.
+#'     (i)  full results files
+#'            1 full results file for pre-vaccination (optional)
+#'            1 full results file for post-vaccination
+#'     (ii) 1 file per country for all runs
+#'            1 file per country for all runs -- pre-vaccination (optional)
+#'            1 file per country for all runs -- post-vaccination
+#' 
+#' @param disease_burden_template_file csv file (required), 
+#'   central disease burden template (vimc format); add column with run_id to get
+#'   stochastic disease burden template
+#' @param centralBurdenResultsFile csv file (required), central disease burden 
+#'   estimates pre and post vaccination
+#' @param psaData data table (required), latin hyper cube sample of input parameters 
+#' @param diseaseBurdenStochasticFolder character string (required),
+#'   stochastic disease burden estimates folder (output)
+#' @param diseaseBurdenStochasticFile character string (required), 
+#'   stochastic disease burden estimates file(s) (output)
+#' @param psa_runs integer (required), simulation runs for sensitivity analysis
+#' @param countryCodes list (optional), If country codes are provided, 
+#'   stochastic burden estimates are generated for these countries. 
+#'   If set to -1, then stochastic burden estimates are generated for the 
+#'   countries included in the central burden estimates. 
+#' @param vaccination_scenario logical (required), generate stochastic burden 
+#'   estimates for (vaccination) or (no vaccination) scenario
+#' 
+#' @return 
+#' @export
+#'  
+EstimateVaccineImpactVimcStochastic <- function (disease_burden_template_file, 
+                                                 centralBurdenResultsFile,
+                                                 psaData,
+                                                 diseaseBurdenStochasticFolder,
+                                                 diseaseBurdenStochasticFile, 
+                                                 psa_runs, 
+                                                 countryCodes = -1, 
+                                                 vaccination_scenario) {
+  
+  # read file -- central disease burden template
+  vimc_template <- fread (disease_burden_template_file)
+  
+  # read in central burden results
+  central_burden <- fread (centralBurdenResultsFile)
+  
+  # initialise empty table to save stochastic results in vimc format
+  header <- vimc_template [0, ]                 # empty table with requisite 
+                                                #   columns
+  header [, run_id := numeric ()]               # add column for run id
+  setcolorder (header, c("disease", "run_id"))  # reorder columns
+  
+  # save header to file for stochastic estimates of disease burden
+  stochastic_file <- paste0 (diseaseBurdenStochasticFolder, 
+                             diseaseBurdenStochasticFile, 
+                             ".csv")
+  fwrite (x      = header, 
+          file   = stochastic_file, 
+          append = FALSE)
+  
+  
+  # extract burden estimates for pre- or post-vaccination
+  if (vaccination_scenario) {
+    
+    # vaccination scenario
+    central_burden <- central_burden [scenario == "post-vaccination"]
+    
+  } else {
+    
+    # no vaccination scenario
+    central_burden <- central_burden [scenario == "pre-vaccination"]
+  }
+  
+  # get country codes
+  if (countryCodes == -1) {
+    countryCodes <- unique (central_burden [, country])
+  } 
+  
+  # generate stochastic burden estimates for each country
+  for (country_code in countryCodes) {
+    
+    # get disease burden template for current country (of this loop)
+    vimc_template_country <- vimc_template [country == country_code]
+    
+    # get central burden estimates for current country 
+    central_burden_country <- central_burden [country == country_code]
+    
+    # get latin hyper cube sample of input parameters for curent country
+    psadat_country <- psaData [country == country_code]
+    
+    # file to save stochastic burden estimates of current country 
+    stochastic_file_country <- paste0 (diseaseBurdenStochasticFolder, 
+                                       diseaseBurdenStochasticFile, 
+                                       "_", country_code, 
+                                       ".csv")
+    
+    # initialise header for stochastic burden estimates
+    stochastic_burden_country <- header
+    
+    # generate post-hoc stochastic estimates for each run
+    for (run_number in 1:psa_runs) {
+      
+      # initialise burden estimate to central burden estimates
+      burden <- central_burden_country
+      
+      # ------------------------------------------------------------------------
+      # probabilistic sensitivity analysis to generate stochastic estimates 
+      
+      # cases -- incidence
+      burden [, inc.cecx := (inc.cecx * psadat_country [run_id == run_number, 
+                                                        incidence_ratio]
+                                      * psadat_country [run_id == run_number, 
+                                                        hpv_distribution_ratio])]
+      
+      # deaths -- mortality 
+      burden [, mort.cecx := (mort.cecx * psadat_country [run_id == run_number, 
+                                                          mortality_ratio]
+                                        * psadat_country [run_id == run_number, 
+                                                          hpv_distribution_ratio])]
+      # prevalence
+      burden [, prev.cecx := (prev.cecx * psadat_country [run_id == run_number, 
+                                                          prevalence_ratio]
+                                        * psadat_country [run_id == run_number, 
+                                                          hpv_distribution_ratio])]
+      
+      # YLL -- premature mortality
+      burden [, lifey := (lifey * psadat_country [run_id == run_number, 
+                                                  mortality_ratio]
+                                * psadat_country [run_id == run_number, 
+                                                  hpv_distribution_ratio])]
+      
+      # ------------------------------------------------------------------------
+      # estimation of YLD -- disability
+      
+      # disability weights for different phases of cervical cancer
+      # (diagnosis & therapy, controlled, metastatic, terminal)
+      dw = list (diag       = psadat_country [run_id == run_number, 
+                                              dw_diagnosis], 
+                 control    = psadat_country [run_id == run_number, 
+                                              dw_control], 
+                 metastatic = psadat_country [run_id == run_number, 
+                                              dw_metastatic], 
+                 terminal   = psadat_country [run_id == run_number, 
+                                              dw_terminal] 
+                 )
+      
+      # duration of different phases of cervical cancer
+      # (diagnosis & therapy, controlled, metastatic, terminal) -- unit in years
+      disability.weights <- "gbd_2017"
+      
+      cecx_duration = list (diag       = data.disability_weights [Source == disability.weights &
+                                                                    Sequela=="diagnosis",
+                                                                  Duration],
+                            metastatic = data.disability_weights [Source == disability.weights &
+                                                                    Sequela=="metastatic",
+                                                                  Duration],
+                            terminal   = data.disability_weights [Source == disability.weights &
+                                                                    Sequela=="terminal",
+                                                                  Duration])
+      # duration of controlled phases is based on remainder of 
+      # time after attributing to other phases
+      
+      # YLD -- disability
+      # combine yld contribution from (incidence, prevalence and mortality) cases
+      burden [, disability := (inc.cecx  * dw$diag * cecx_duration$diag) + 
+                              (prev.cecx * dw$control) +
+                              (mort.cecx * ( (dw$metastatic * cecx_duration$metastatic) +
+                                             (dw$terminal   * cecx_duration$terminal) ) ) ]
 
-  # 112 csv files with stochastic estimates; 1 file per country
+      # ------------------------------------------------------------------------
+      
+      # ------------------------------------------------------------------------
+      
+      # convert results to vimc format
+      convert_results <- OutputVimc (DT            = burden, 
+                                     calendar_year = TRUE, 
+                                     vimc_template = vimc_template_country)
+      
+      # drop scenario (post-vaccination or pre-vaccination) column
+      convert_results [, scenario := NULL]
+      
+      # add column for run id
+      convert_results [, run_id := run_number]
+      
+      # add stochastic burden estimate adjusted by sensitivity analysis to the
+      # stochastic burden table of all runs
+      stochastic_burden_country <- rbind (stochastic_burden_country, 
+                                          convert_results,
+                                          use.names = TRUE)
+      
+    } # end of -- for (run_id in 1:psa_runs)
+    
+    # save stochastic burden estimates of current country
+    fwrite (x      = stochastic_burden_country, 
+            file   = stochastic_file_country, 
+            append = FALSE)
+    
+    # save stochastic burden estimates of current country to larger file with 
+    # stochastic burden estimates of all countries
+    fwrite (x      = stochastic_burden_country, 
+            file   = stochastic_file, 
+            append = TRUE)
+    
+  } # end of -- for (country_code in countryCodes)
 
-
+  return ()
+  
 } # end of function -- EstimateVaccineImpactVimcStochastic
 #-------------------------------------------------------------------------------
 
@@ -204,8 +406,8 @@ central_burden_template_file <- paste0 ("input/central-burden-template.",
 
 #-------------------------------------------------------------------------------
 # initialisation for probabilistic sensitivity analysis
-run_psa    <- TRUE  # logical, run/not run PSA
-psa        <- 200   # number of runs for psa
+run_psa    <- TRUE   # logical, run/not run PSA
+psa_runs   <- 200    # number of runs for psa
 seed_state <- 1
 vaccine    <- "4vHPV"
 
@@ -227,26 +429,25 @@ if (run_psa) {
   # create psa data for probabilistic sensitivity analysis
   psadat_list <- CreatePsaData (country_codes    = country_codes,
                                 vaccine          = vaccine,
-                                psa              = psa,
+                                psa_runs         = psa_runs,
                                 seed_state       = seed_state, 
                                 psadat_file      = psadat_file,
                                 psadat_vimc_file = psadat_vimc_file)
 }
 #-------------------------------------------------------------------------------
 
-#-------------------------------------------------------------------------------
-# Generate vaccine impact estimates (central run)
-
 # scenarios
-# scenarios <- c ("hpv-routine-default",
-#                 "hpv-routine-bestcase",
-#                 "hpv-campaign-default",
-#                 "hpv-campaign-bestcase")
-scenarios <- c ("hpv-routine-default")  # DEGBUG -- remove this line later
+scenarios <- c ("hpv-routine-default",
+                "hpv-routine-bestcase",
+                "hpv-campaign-default",
+                "hpv-campaign-bestcase")
+# scenarios <- c ("hpv-routine-default")  # DEBUG -- comment this line later
 
 # loop through different scenarios
 for (scenario in scenarios) {
   
+  #-----------------------------------------------------------------------------
+  # Generate vaccine impact estimates (central run)
   tic ()  # start timer
   
   # vaccination coverage filename
@@ -318,8 +519,13 @@ for (scenario in scenarios) {
   )
   
   toc ()  # note current timer and compute elapsed time
+  #-----------------------------------------------------------------------------
   
-  # probabilistic sensitivity analysis
+  #-----------------------------------------------------------------------------
+  # Generate vaccine impact estimates (stochastic run - vaccination scenario)
+  tic ()  # start timer
+  
+  # probabilistic sensitivity analysis for vaccination scenarios
   if (run_psa) {
     
     # directory for stochastic burden files
@@ -327,36 +533,63 @@ for (scenario in scenarios) {
                                       scenario, "/")
 
     
-    # loop through psa runs -- instead, loop through each country
-    for (run_i in 1:psa) {
-      
-      # filename for stochastic burden estimates
-      stochastic_burden_novaccination_file <- paste0 ("stochastic-burden-novaccination_", 
-                                                      touchstone, "_", 
-                                                      scenario, "_", 
-                                                      run_i, ".csv")
-      
-      stochastic_burden_vaccination_file <- paste0 ("stochastic-burden-vaccination_", 
-                                                    touchstone, "_", 
-                                                    scenario, "_", 
-                                                    run_i, ".csv")
-      
-      
-      
-      # Generate vaccine impact estimates (sensitivity analysis/stochastic runs)
-      EstimateVaccineImpactVimcStochastic (
-        resultsFile = central_burden_results_file,
-        psaDataFile,
-        countryCode,
-        diseaseBurdenStochasticFile,
-        fileNumber) 
-      
-      # 112 csv files with stochastic estimates; 1 file per country
-      
-    }
+    # filename for stochastic burden estimates (without .csv at the end)
+    stochastic_burden_vaccination_file <- paste0 ("stochastic-burden-vaccination_", 
+                                                  touchstone, "_", 
+                                                  scenario)
+    
+    # Generate vaccine impact estimates (sensitivity analysis/stochastic runs)
+    # for vaccination scenario
+    EstimateVaccineImpactVimcStochastic (
+      disease_burden_template_file  = central_burden_template_file,
+      centralBurdenResultsFile      = central_burden_results_file,
+      psaData                       = psadat_list [["psadat"]],
+      diseaseBurdenStochasticFolder = stochastic_burden_dir,
+      diseaseBurdenStochasticFile   = stochastic_burden_vaccination_file, 
+      psa_runs                      = psa_runs, 
+      countryCodes                  = -1,
+      vaccination_scenario          = TRUE
+      ) 
+  }
   
+  toc ()
+  #-----------------------------------------------------------------------------
   
 } # end of loop -- for (scenario in scenarios)
+
+
+#-----------------------------------------------------------------------------
+# Generate vaccine impact estimates (stochastic run - vaccination scenario)
+tic ()
+
+# probabilistic sensitivity analysis for no vaccination scenario
+if (run_psa) {
+
+  # directory for stochastic burden files
+  stochastic_burden_dir <-  paste0 ("output_psa/",
+                                    "hpv-no-vaccination", "/")
+  
+  # filename for stochastic burden estimates (without .csv at the end)
+  stochastic_burden_novaccination_file <- paste0 ("stochastic-burden-novaccination_", 
+                                                  touchstone, "_", 
+                                                  "hpv-no-vaccination")
+  
+  # Generate vaccine impact estimates (sensitivity analysis/stochastic runs)
+  # for no vaccination scenario
+  EstimateVaccineImpactVimcStochastic (
+    disease_burden_template_file  = central_burden_template_file,
+    centralBurdenResultsFile      = central_burden_results_file,
+    psaData                       = psadat_list [["psadat"]],
+    diseaseBurdenStochasticFolder = stochastic_burden_dir,
+    diseaseBurdenStochasticFile   = stochastic_burden_novaccination_file, 
+    psa_runs                      = psa_runs, 
+    countryCodes                  = -1,
+    vaccination_scenario          = FALSE
+  ) 
+}
+
+toc ()
+#-----------------------------------------------------------------------------
 
 
 #-------------------------------------------------------------------------------
